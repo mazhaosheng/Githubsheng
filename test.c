678687422f23c33d37a27698f4fe8eba9c1f49c2
mazhaosheng <66000976+mazhaosheng@users.noreply.github.com>
2020-05-27 19:18:50 +0800

    Update test.c

--- test.c
+++ test.c
@@ -5,6 +5,6 @@ int main(int argc, const char *argv[])
 	printf("hello world\n");
 	printf("develop*******************\n");
 	printf("come on!hurry up~\n");
-
+	printf("No!!!I'm very hungry~~");
 	return 0;
 }
